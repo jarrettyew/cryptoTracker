3c58206c77fe8c947bb1480c1d3eb505f39cf9b0
Jarrett Yew <jarrett.yew@gmail.com>
2022-02-20 11:05:07 +0800

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -33,8 +33,21 @@ const App = () => {
               </Routes>
             </div>
           </Layout>
+          <div className="footer">
+            <Typography.Title
+              level={5}
+              style={{ color: "white", textAlign: "center" }}
+            >
+              Copyright © 2022 <Link to="/">Crypto Tracker Inc.</Link> <br />
+              All Rights Reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
         </div>
-        <div className="footer"></div>
       </div>
     </div>
   );
